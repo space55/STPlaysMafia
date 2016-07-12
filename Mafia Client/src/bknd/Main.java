1475a10654dee6014fbcaa9639a572a522eb2a81
space55 <elg.nugent@gmail.com>
2016-07-12 09:15:12 -0400

    Added main class & method

---
+++ Mafia Client/src/bknd/Main.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (C) Eamonn Nugent - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by Eamonn Nugent <elg.nugent@gmail.com>, 2016
+ *******************************************************************************/
+
+package bknd;
+
+public class Main
+{
+	public static Client c;
+
+	public static void main(String[] args)
+	{
+		c = new Client();
+		c.run();
+	}
+}
